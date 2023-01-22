6a95191b88150f5c6b83cb96716bdce24c1677bc
Mister246 <86894096+Mister246@users.noreply.github.com>
2023-01-22 10:03:01 -0500

    Improved Button Highlight Script
    - Made the script work without needing to reference ListOfPlaylistsScript.cs
    - Cleaned up existing code.
    - Added documentation.
    
    At the moment however the script makes it so that only one button can be highlighted at a time, even between different lists. Try to make it so each list can have a single highlight independently.

--- ButtonHighlightScript.cs
+++ ButtonHighlightScript.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonHighlightScript : MonoBehaviour
-// Makes a button's background and text change color upon being clicked
+// Makes a button's background and text change color upon being clicked.
+// Ensures only one instance of this button is highlighted at a time and dehighlights the previously selected button.
 {
-    public Button button;
-    public bool highlighted;
+    Button button; // button component of this game object
+    public bool highlighted; // bool to determine if this is highlighted
+    static public ButtonHighlightScript currentlyHighlightedButton; // global static reference to whichever button is currently highlighted
+    Color defaultBackgroundColor = new(0.235f, 0.235f, 0.235f); // color is #3C3C3C
+
     void Start()
     {
         button = GetComponent<Button>();
-        button.onClick.AddListener(delegate { HighlightOnSelect(); } ); 
+        button.onClick.AddListener(delegate { HighlightOnSelect(); } );
+        // make button execute HighlightOnSelect() whenever it is clicked on
         highlighted = false;
+        // when a button spawns it is not highlighted
     }
-    public void HighlightOnSelect()
+
+    void HighlightOnSelect()
     {
         if (!highlighted)
         {
-            GetComponent<Image>().color = Color.white;
-            // change button background color to white
-            button.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0.235f, 0.235f, 0.235f);
-            // change button text to #3C3C3C
-            highlighted = true;
-            ListOfPlaylistsScript.currentlyHighlightedButton = this;
+            DehighlightPreviouslySelectedButton();
+            GetComponent<Image>().color = Color.white; // change button background color to white
+            button.GetComponentInChildren<TextMeshProUGUI>().color = defaultBackgroundColor; // change button text to #3C3C3C
+            highlighted = true; // button is now highlighted
+            currentlyHighlightedButton = this; // create a reference to this button so it can be dehighlighted when another button is highlighted
+        }
+    }
+
+    void DehighlightPreviouslySelectedButton()
+    {
+        if (currentlyHighlightedButton != null)
+        // if a button was previously selected
+        {
+            currentlyHighlightedButton.button.GetComponentInChildren<TextMeshProUGUI>().color = Color.white; // change button text back to white
+            currentlyHighlightedButton.button.GetComponent<Image>().color = defaultBackgroundColor; // change button background back to #3C3C3C
+            currentlyHighlightedButton.highlighted = false; // button is now no longer highlighted
         }
     }
 }

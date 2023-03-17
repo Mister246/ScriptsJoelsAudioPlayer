05ff20075ad50a6f3cd3c12ac232f69d240d0835
Mister246 <86894096+Mister246@users.noreply.github.com>
2023-03-17 15:50:33 -0400

    Added Progress Text

--- ProgressBarScript.cs
+++ ProgressBarScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBarScript : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class ProgressBarScript : MonoBehaviour
     // in the editor, knob starts at x = -500 and ends at x = 500
     // units in editor don't seem to evenly equate to units in script however
     // for the logic of this script, knob starts at x = 0 and ends at x = 800
+    static public Text progressText; // display of clip.time out of clip.length
 
     static float startingPosition; // x = 0 for the knob
     float audioProgressPercentage; // represents how much of the audio file has been played
@@ -17,12 +19,15 @@ public class ProgressBarScript : MonoBehaviour
     {
         knob = GameObject.Find("Knob");
         startingPosition = knob.transform.position.x; // x = 0
+        
+        progressText = FindObjectOfType<Text>();
     }
 
     void Update()
     {
         if (!ControlPanelScript.audioSource.isPlaying) return;
 
+        DisplayProgressInTime();
         audioProgressPercentage = ControlPanelScript.audioSource.time / ControlPanelScript.audioSource.clip.length; // determine how much of the audio file has been played
         knob.transform.position = new Vector3(startingPosition + (800 * audioProgressPercentage), knob.transform.position.y, knob.transform.position.z); // move knob according to percentage
     }
@@ -31,5 +36,21 @@ public class ProgressBarScript : MonoBehaviour
     // resets knob back to starting position
     {
         knob.transform.position = new Vector3(startingPosition, knob.transform.position.y, knob.transform.position.z);
+    }
+
+    static public void DisplayProgressInTime()
+    {
+        int seconds = (int)ControlPanelScript.audioSource.time;
+        int minutes = seconds / 60;
+        seconds %= 60; // seconds must be divided after getting minutes to allow minutes to be calculated
+
+        if (seconds <= 10)
+        {
+            progressText.text = $"{minutes}:0{seconds} / ";
+        }
+        else
+        {
+            progressText.text = $"{minutes}:{seconds} / ";
+        }
     }
 }
